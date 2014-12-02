874b6bb1b8b6aa8c8c6b6eda1f325ec442c86dec
Don Irwin <don@falconwebtech.com>
2014-12-01 21:44:48 -0700

    Update FracCalcV2.java

--- src/FracCalcV2.java
+++ src/FracCalcV2.java
@@ -1,1 +1,13 @@
 // Complete the main class FracCalcV2 to create two instances of RationalNumber and use the methods
+
+// in your main method...
+//   create two instances of RationalNumber frac1 and frac2 with values 3/6 and 2/8
+//   print each RationalNumber using the toString() method
+//   - add frac2 to frac1, i.e. frac1.add(frac2);
+//     -- print frac1 using toString()
+//   - subtract frac2 from frac1
+//     -- print frac1
+//   - multiply frac1 by frac2
+//     -- print frac1
+//   - divide frac1 by frac2
+//     -- print frac1
